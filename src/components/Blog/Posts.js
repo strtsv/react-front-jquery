e5aee1fcd30e6843003447be6968ec42d8a5e9dd
jsusilo <enr@bk.ru>
2020-08-22 00:13:40 +0500

    blog posts

---
+++ src/components/Blog/Posts.js
@@ -0,0 +1,311 @@
+import React from "react";
+
+import i1 from "../../assets/images/blog-1-369x253.jpg";
+import i2 from "../../assets/images/blog-2-369x253.jpg";
+import i3 from "../../assets/images/blog-3-369x253.jpg";
+import i4 from "../../assets/images/blog-4-369x253.jpg";
+import i5 from "../../assets/images/blog-5-369x253.jpg";
+import i6 from "../../assets/images/blog-6-369x253.jpg";
+
+class Posts extends React.Component {
+  render() {
+    return (
+      <section className="section section-md">
+        <div className="container">
+          <div className="row row-50 row-xl-70">
+            <div className="col-sm-6 col-lg-4">
+              <article className="post-classic">
+                <a className="post-classic-media" href="blog-post.html">
+                  <img
+                    className="post-classic-image"
+                    src={i1}
+                    alt
+                    width={369}
+                    height={253}
+                  />
+                </a>
+                <h4 className="post-classic-title">
+                  <a href="blog-post.html">
+                    8 Surprising Predictions About the Future of Work
+                  </a>
+                </h4>
+                <time className="post-classic-time" dateTime={2019}>
+                  November 23, 2019
+                </time>
+                <div className="post-classic-text">
+                  <p>
+                    There’s no denying that the landscape of work is changing.
+                    More and more companies are adopting{" "}
+                  </p>
+                </div>
+                <ul className="post-classic-meta">
+                  <li>
+                    <a href="blog-post.html">
+                      {" "}
+                      <span className="icon mdi mdi-comment-outline" />
+                      <span>3 Comments</span>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <span className="icon mdi mdi-thumb-up-outline" />
+                      <span>30 Likes</span>
+                    </a>
+                  </li>
+                </ul>
+              </article>
+            </div>
+            <div className="col-sm-6 col-lg-4">
+              <article className="post-classic">
+                <a className="post-classic-media" href="blog-post.html">
+                  <img
+                    className="post-classic-image"
+                    src={i2}
+                    alt
+                    width={369}
+                    height={253}
+                  />
+                </a>
+                <h4 className="post-classic-title">
+                  <a href="blog-post.html">
+                    Job Search Success: Finding a Business Development Job
+                  </a>
+                </h4>
+                <time className="post-classic-time" dateTime={2019}>
+                  November 23, 2019
+                </time>
+                <div className="post-classic-text">
+                  <p>
+                    Business development professionals are at the heart of all
+                    sorts of organizations, from startups to
+                  </p>
+                </div>
+                <ul className="post-classic-meta">
+                  <li>
+                    <a href="blog-post.html">
+                      {" "}
+                      <span className="icon mdi mdi-comment-outline" />
+                      <span>3 Comments</span>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <span className="icon mdi mdi-thumb-up-outline" />
+                      <span>30 Likes</span>
+                    </a>
+                  </li>
+                </ul>
+              </article>
+            </div>
+            <div className="col-sm-6 col-lg-4">
+              <article className="post-classic">
+                <a className="post-classic-media" href="blog-post.html">
+                  <img
+                    className="post-classic-image"
+                    src={i3}
+                    alt
+                    width={369}
+                    height={253}
+                  />
+                </a>
+                <h4 className="post-classic-title">
+                  <a href="blog-post.html">
+                    How to Impress Your Future Employer
+                  </a>
+                </h4>
+                <time className="post-classic-time" dateTime={2019}>
+                  November 23, 2019
+                </time>
+                <div className="post-classic-text">
+                  <p>
+                    You’re committed to your job search, and you use every ounce
+                    of your free time you have to comb{" "}
+                  </p>
+                </div>
+                <ul className="post-classic-meta">
+                  <li>
+                    <a href="blog-post.html">
+                      {" "}
+                      <span className="icon mdi mdi-comment-outline" />
+                      <span>3 Comments</span>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <span className="icon mdi mdi-thumb-up-outline" />
+                      <span>30 Likes</span>
+                    </a>
+                  </li>
+                </ul>
+              </article>
+            </div>
+            <div className="col-sm-6 col-lg-4">
+              <article className="post-classic">
+                <a className="post-classic-media" href="blog-post.html">
+                  <img
+                    className="post-classic-image"
+                    src={i4}
+                    alt
+                    width={369}
+                    height={253}
+                  />
+                </a>
+                <h4 className="post-classic-title">
+                  <a href="blog-post.html">
+                    Organize Your Workspace with These Tips
+                  </a>
+                </h4>
+                <time className="post-classic-time" dateTime={2019}>
+                  November 23, 2019
+                </time>
+                <div className="post-classic-text">
+                  <p>
+                    What’s the hardest working location in your house? If you’re
+                    like many workers, it’s your office{" "}
+                  </p>
+                </div>
+                <ul className="post-classic-meta">
+                  <li>
+                    <a href="blog-post.html">
+                      {" "}
+                      <span className="icon mdi mdi-comment-outline" />
+                      <span>3 Comments</span>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <span className="icon mdi mdi-thumb-up-outline" />
+                      <span>30 Likes</span>
+                    </a>
+                  </li>
+                </ul>
+              </article>
+            </div>
+            <div className="col-sm-6 col-lg-4">
+              <article className="post-classic">
+                <a className="post-classic-media" href="blog-post.html">
+                  <img
+                    className="post-classic-image"
+                    src={i5}
+                    alt
+                    width={369}
+                    height={253}
+                  />
+                </a>
+                <h4 className="post-classic-title">
+                  <a href="blog-post.html">
+                    Finding a Remote Editing Job to Satisfy Your Needs
+                  </a>
+                </h4>
+                <time className="post-classic-time" dateTime={2019}>
+                  November 23, 2019
+                </time>
+                <div className="post-classic-text">
+                  <p>
+                    In this post, we’d like to focus on members who found remote
+                    editing jobs, allowing them to work{" "}
+                  </p>
+                </div>
+                <ul className="post-classic-meta">
+                  <li>
+                    <a href="blog-post.html">
+                      {" "}
+                      <span className="icon mdi mdi-comment-outline" />
+                      <span>3 Comments</span>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <span className="icon mdi mdi-thumb-up-outline" />
+                      <span>30 Likes</span>
+                    </a>
+                  </li>
+                </ul>
+              </article>
+            </div>
+            <div className="col-sm-6 col-lg-4">
+              <article className="post-classic">
+                <a className="post-classic-media" href="blog-post.html">
+                  <img
+                    className="post-classic-image"
+                    src={i6}
+                    alt
+                    width={369}
+                    height={253}
+                  />
+                </a>
+                <h4 className="post-classic-title">
+                  <a href="blog-post.html">
+                    Online Teaching: Theory and Basics
+                  </a>
+                </h4>
+                <time className="post-classic-time" dateTime={2019}>
+                  November 23, 2019
+                </time>
+                <div className="post-classic-text">
+                  <p>
+                    Have you been considering an online teacher job but don’t
+                    know exactly how to get started? A great first
+                  </p>
+                </div>
+                <ul className="post-classic-meta">
+                  <li>
+                    <a href="blog-post.html">
+                      {" "}
+                      <span className="icon mdi mdi-comment-outline" />
+                      <span>3 Comments</span>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <span className="icon mdi mdi-thumb-up-outline" />
+                      <span>30 Likes</span>
+                    </a>
+                  </li>
+                </ul>
+              </article>
+            </div>
+          </div>
+          {}
+          <nav
+            className="pagination-outer text-center"
+            aria-label="Page navigation"
+          >
+            <div className="pagination">
+              <div className="page-item page-item-control">
+                <a className="page-link" href="#" aria-label="Previous">
+                  Prev
+                </a>
+              </div>
+              <div className="page-item">
+                <a className="page-link" href="#">
+                  1
+                </a>
+              </div>
+              <div className="page-item">
+                <a className="page-link" href="#">
+                  2
+                </a>
+              </div>
+              <div className="page-item active">
+                <span className="page-link">3</span>
+              </div>
+              <div className="page-item">
+                <a className="page-link" href="#">
+                  4
+                </a>
+              </div>
+              <div className="page-item page-item-control">
+                <a className="page-link" href="#" aria-label="Next">
+                  Next
+                </a>
+              </div>
+            </div>
+          </nav>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default Posts;
